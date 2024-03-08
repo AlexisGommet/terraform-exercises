249f6874f86da27b57d3e52df6bcad02f56c63a0
agommet <agommet@ippon.fr>
2024-03-08 16:02:26 +0100

    ex4 works

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -39,9 +39,9 @@ resource "aws_security_group" "bastion_sg" {
 
 resource "aws_security_group_rule" "alb_ingress" {
   type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.alb_sg.id
 }
